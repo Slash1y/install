24dc6a81b9f85d540f028bb5021dd0f7a217c589
Lukas <lukas.stauersbol@gmail.com>
2020-09-18 16:46:42 +0200

    Update v4

--- setup.sh
+++ setup.sh
@@ -2,14 +2,6 @@
 #This script is for downloading and installing basic functions for the machine.
 #This will mostly only work on Ubuntu 18.04 LTS and newer.
 #Check if system is Ubuntu or Debian
-if [ "`lsb_release -is`" == "Ubuntu" ] || [ "`lsb_release -is`" == "Debian" ] && ["`id -un`" == "root"]
-then
-  Install
-else
-  echo "You are not using a Ubuntu or Debian machine."
-  echo "You are also not logged in as root. Please try again while logged in as root."
-fi
-
 function Install {
   # Install XFCE4-Desktop
   echo "Installing XFCE4-Desktop with extras."
@@ -23,3 +15,11 @@ function Install {
   echo "Installing CURL, TAR, UNZIP and GIT"
   apt-get install -y curl tar unzip git
 }
+
+if [ "`lsb_release -is`" == "Ubuntu" ] || [ "`lsb_release -is`" == "Debian" ]
+then
+  Install
+else
+  echo "You are not using a Ubuntu or Debian machine."
+  echo "You are also not logged in as root. Please try again while logged in as root."
+fi
